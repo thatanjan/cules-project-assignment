8b1be3e61bed5b6c6e076b6b3931b17769d34f98
thatanjan <thatanjan@gmail.com>
2022-05-12 18:56:38 +0600

    Material ui setup is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-const Home = () => {
-	return <div>Home</div>
-}
+import Typography from '@mui/material/Typography'
+
+const Home = () => (
+	<div>
+		<Typography>Hello world</Typography>
+	</div>
+)
 
 export default Home
