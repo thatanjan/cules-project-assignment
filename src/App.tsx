3faf03092e3f1941e3d54ce1e27dc616786e2273
thatanjan <thatanjan@gmail.com>
2022-05-13 13:44:38 +0600

    Project data will not be initialized in the app file. It will be inside the ProjectCardsShow. Data will be stored inside states.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import { useEffect } from 'react'
 import Typography from '@mui/material/Typography'
-
-import projectData from 'data.json'
 
 import ProjectCardsShow from 'Components/Project/ProjectCardsShow'
 
-import { getProjectData, storeProjectData } from 'utils/projectDataUtils'
+const Home = () => (
+	<>
+		<Typography variant='h1' align='center' sx={{ padding: '2rem 0' }}>
+			Cules Project
+		</Typography>
 
-const Home = () => {
-	useEffect(() => {
-		if (!getProjectData()) storeProjectData(projectData)
-	}, [])
-
-	return (
-		<>
-			<Typography variant='h1' align='center' sx={{ padding: '2rem 0' }}>
-				Cules Project
-			</Typography>
-
-			<ProjectCardsShow />
-		</>
-	)
-}
+		<ProjectCardsShow />
+	</>
+)
 
 export default Home
--- src/Components/Project/ProjectCardsShow.tsx
+++ src/Components/Project/ProjectCardsShow.tsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from 'react'
 import { nanoid } from 'nanoid'
+
+import { getProjectData, storeProjectData } from 'utils/projectDataUtils'
+
+import { Projects } from 'types/Project'
+
+import initialData from 'data.json'
 
 import ProjectCard from './ProjectCard'
 
 type Props = {}
 
-const ProjectCardsShow = (props: Props) => (
-	<div>
-		{Array(10)
-			.fill(0)
-			.map(() => (
+const ProjectCardsShow = (props: Props) => {
+	const [projectData, setProjectData] = useState<Projects>([])
+
+	useEffect(() => {
+		let data = getProjectData()
+
+		if (!data) {
+			storeProjectData(initialData)
+			data = initialData
+		}
+
+		setProjectData(data as Projects)
+	}, [])
+
+	return (
+		<div>
+			{projectData.map(() => (
 				<ProjectCard key={nanoid()} />
 			))}
-	</div>
-)
+		</div>
+	)
+}
 
 export default ProjectCardsShow
