9a398ba16d2d6b36cc9c973ff4924f61c9a03ad7
thatanjan <thatanjan@gmail.com>
2022-05-13 11:16:20 +0600

    responsive theme is added to the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+
+import theme from 'theme'
 
 import App from './App'
 
@@ -8,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<React.StrictMode>
-		<CssBaseline />
-		<App />
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 )
