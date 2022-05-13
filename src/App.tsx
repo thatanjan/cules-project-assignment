fdd91b1f0bde0498cfd6ed804e88c2ab93509068
thatanjan <thatanjan@gmail.com>
2022-05-14 00:12:42 +0600

    The whole app will be now at the center

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 import ProjectCardsShow from 'Components/Project/ProjectCardsShow'
 
 const Home = () => (
-	<>
+	<Box sx={{ width: '90%', maxWidth: '80rem', m: '0 auto' }}>
 		<Typography variant='h1' align='center' sx={{ padding: '2rem 0' }}>
 			Cules Project
 		</Typography>
 
 		<ProjectCardsShow />
-	</>
+	</Box>
 )
 
 export default Home
