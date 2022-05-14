8d4a8d117d10e7745d9c9ac44f17b7b0f8bda2a8
thatanjan <thatanjan@gmail.com>
2022-05-14 14:21:02 +0600

    Two hooks created to get the project and sortType

--- src/redux/hooks/baseHooks.ts
+++ src/redux/hooks/baseHooks.ts
@@ -3,3 +3,9 @@ import type { RootState, AppDispatch } from '../store'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export const useProjects = () =>
+	useAppSelector(state => state.projects.projects)
+
+export const useSortType = () =>
+	useAppSelector(state => state.projects.sortType)
