5dde259e29b89b2120c897b9c237ceeb084228bc
thatanjan <thatanjan@gmail.com>
2022-05-14 14:37:08 +0600

    Unnecessary codes are removed from ProjectCardsShow component

--- src/Components/Project/ProjectCardsShow.tsx
+++ src/Components/Project/ProjectCardsShow.tsx
@@ -1,12 +1,4 @@
-import { useEffect, useState } from 'react'
-import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
-
-import { getProjectData, storeProjectData } from 'utils/projectDataUtils'
-
-import { Projects, Project } from 'types/Project'
-
-import initialData from 'data.json'
 
 import { useProjects } from 'redux/hooks/baseHooks'
 
@@ -15,38 +7,11 @@ import AddProject from './AddProject'
 import SortProjects from './SortProjects'
 
 const ProjectCardsShow = () => {
-	const [projectData, setProjectData] = useState<Projects>([])
-
 	const projects = useProjects()
-
-	useEffect(() => {
-		let data = getProjectData()
-
-		if (!data) {
-			storeProjectData(initialData)
-			data = initialData
-		}
-
-		setProjectData(data as Projects)
-	}, [])
-
-	const addNewProject = (project: Project) => {
-		const newProjects = projectData.concat(project)
-
-		setProjectData(newProjects)
-		storeProjectData(newProjects)
-	}
-
-	const deleteProject = (id: string) => {
-		const remaining = projectData.filter(project => project.id !== id)
-
-		setProjectData(remaining)
-		storeProjectData(remaining)
-	}
 
 	return (
 		<div>
-			<AddProject addNewProject={addNewProject} />
+			<AddProject />
 
 			<SortProjects />
 
@@ -62,7 +27,7 @@ const ProjectCardsShow = () => {
 				}}
 			>
 				{projects.map(project => (
-					<ProjectCard deleteProject={deleteProject} {...project} key={project.id} />
+					<ProjectCard {...project} key={project.id} />
 				))}
 			</Box>
 		</div>
