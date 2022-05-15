fde37f68494caead7454b362c3da650acee542cc
thatanjan <thatanjan@gmail.com>
2022-05-15 23:28:00 +0600

    addProject reducer is tested

--- src/redux/reducers/projectsReducer.test.ts
+++ src/redux/reducers/projectsReducer.test.ts
@@ -1,6 +1,8 @@
 import data from 'data.json'
 import { sortingOptions } from 'globalVars'
 
+import { Projects } from 'types/Project'
+
 import projectsReducer, { addProject } from './projectsReducer'
 
 test('should return the initial state', () => {
@@ -8,6 +10,22 @@ test('should return the initial state', () => {
 
 	expect(projects).toEqual(data)
 	expect(sortType).toEqual(sortingOptions.dateDesc)
+})
+
+test('should add new Project', () => {
+	const initialState = projectsReducer(undefined, {})
+
+	const newProject = {
+		id: 'a87e8618-7392-4ac2-b4d0-c6b9b8fb3304',
+		name: 'Redux',
+		url: 'https://github.com/facebook/react',
+		rating: 5,
+		created_at: '2021-11-19T13:46:36.211Z',
+	}
+
+	const laterState = projectsReducer(initialState, addProject(newProject))
+
+	expect(laterState.projects).toEqual(initialState.projects.concat(newProject))
 })
 
 test('should first', () => {})
